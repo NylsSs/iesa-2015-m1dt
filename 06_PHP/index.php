c0be7a26654464cfd5b90b4817a832c4868000aa
NylsSs <nilsredoulez@gmail.com>
2015-03-19 10:34:57 +0100

    Portée d'une variable dans une fonction

--- 06_PHP/index.php
+++ 06_PHP/index.php
@@ -1,12 +1,18 @@
 <!Doctype html>
 <html lang="fr">
-<head>
-	<meta charset="utf-8">
-	<title>PHP</title>
-</head>
-<body>
-	<?php
-		phpinfo();
-	?>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<title>PHP</title>
+	</head>
+	<body>
+		<?php
+			$a = "Bonjour";
+			$$a = "Monde";
+			
+			echo $a . "<br />";
+			echo $$a . "<br />";
+			echo ${$a} . "<br />";
+			echo $Bonjour . "<br />";
+		?>
+	</body>
 </html>
